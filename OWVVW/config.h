e402ab9600af7deb57e54a7838886ee73d5c53d0
Daniel Rhoades <daniel@danielrhoades.com>
2025-01-09 16:53:17 +0000

    Update config.h

--- OWVVW/config.h
+++ OWVVW/config.h
@@ -6,6 +6,7 @@
 #define USB_SUSPEND_WAKEUP_DELAY 0
 #define SERIAL_NUMBER "OWVVW/GGOZX3"
 #define LAYER_STATE_16BIT
+#define COMBO_COUNT 38
 #define HCS(report) host_consumer_send(record->event.pressed ? report : 0); return false
 
 #define RGB_MATRIX_STARTUP_SPD 60
